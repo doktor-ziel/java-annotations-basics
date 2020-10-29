ef81b3a807e8fcf53595ae93a2f178f441f29c89
Doktor Ziel <doktor@zielony-backlog.pl>
2020-10-29 19:26:32 +0100

    Atributes of @Deprecated annotation

---
+++ 03-deprecated/Library.java
@@ -0,0 +1,6 @@
+@Deprecated(since = "1.0", forRemoval = true)
+public class Library {
+	public double areaOfCircle(double radius) {
+		return Math.PI * radius * radius;
+	}
+}
---
+++ 03-deprecated/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+	public static void main(String [] args) {
+		Library l = new Library();
+		System.out.println(l.areaOfCircle(3.0));
+	}
+}

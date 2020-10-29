288a6ccc715bedc1e6f48f969637f4b0d533a12e
Doktor Ziel <doktor@zielony-backlog.pl>
2020-10-29 19:25:30 +0100

    Adnotation override causes compilation error

---
+++ 01-override/SubClass.java
@@ -0,0 +1,7 @@
+
+public class SubClass extends SuperClass {
+	@Override
+	public void f(long a) {
+		System.out.println("Jestes w klasie podrzednej: " + a);
+	}
+}
---
+++ 01-override/SuperClass.java
@@ -0,0 +1,6 @@
+
+public class SuperClass {
+	public void f(int a) {
+		System.out.println("Jestes w klasie nadrzednej: " + a);
+	}
+}
